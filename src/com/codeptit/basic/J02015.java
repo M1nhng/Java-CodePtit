ab35cb95bbab116a9139b9babd0cecc84c376f7d
M1nhng <nguyenchuminh223@gmail.com>
2025-08-26 16:24:28 +0700

    Java

--- src/com/codeptit/basic/J02015.java
+++ src/com/codeptit/basic/J02015.java
@@ -1,5 +1,9 @@
 package com.codeptit.basic;
 
+import java.util.Scanner;
+
 public class J02015 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
