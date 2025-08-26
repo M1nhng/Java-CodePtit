d3cd0c68bb87ce20c62758943fe46dedae146dd4
M1nhng <nguyenchuminh223@gmail.com>
2025-08-26 17:15:17 +0700

    Java

--- src/com/codeptit/basic/J02017.java
+++ src/com/codeptit/basic/J02017.java
@@ -1,9 +1,29 @@
 package com.codeptit.basic;
 
-import java.util.Scanner;
+import java.util.*;
+
+class ThuGonDaySo {
+    static int thuGon(ArrayList<Integer> list) {
+        Stack<Integer> st = new Stack<>();
+        for (int x : list) {
+            if (!st.isEmpty() && (st.peek() + x) % 2 == 0) {
+                st.pop(); 
+            } else {
+                st.push(x);
+            }
+        }
+        return st.size(); 
+    }
+}
 
 public class J02017 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+        System.out.println(ThuGonDaySo.thuGon(list));
     }
 }
