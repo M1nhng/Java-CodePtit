0b18f58f47bc6c25ca99473fc696442803f06756
M1nhng <nguyenchuminh223@gmail.com>
2025-08-31 20:30:29 +0700

    Java

--- src/com/codeptit/basic/J01013.java
+++ src/com/codeptit/basic/J01013.java
@@ -1,34 +1,44 @@
-// package com.codeptit.basic;
+package com.codeptit.basic;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// class Check {
-//     static int checknum(int n) {
-//         for (int i = 2; i <= n; i++) {
-//             if (n % i == 0) {
-//                 sum += i;
-//                 int j = n / i;
-//                 if(n % j == 0){
-//                     sum += j;
-//                 }
-//             }
-//             if(n == 1){
-//                 return n;
-//             }
-//         }
-//         return sum;
-//     }
-// }
-// public class J01013 {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         Check check = new Check();
-//         int t = Integer.parseInt(scanner.nextLine());
-//         int sum = 0;
-//         while(t--> 0){
-//             sum += 
-            
-//         }
-//         System.out.println(Check.checknum(n));
-//     }
-// }
+public class J01013 {
+    static final int MAX = 2000000;
+    static int[] spf = new int[MAX + 1]; 
+
+
+    static void sieve() {
+        for (int i = 2; i <= MAX; i++)
+            spf[i] = i;
+        for (int i = 2; i * i <= MAX; i++) {
+            if (spf[i] == i) { 
+                for (int j = i * i; j <= MAX; j += i) {
+                    if (spf[j] == j)
+                        spf[j] = i;
+                }
+            }
+        }
+    }
+
+    static int checknum(int n) {
+        int sum = 0;
+        while (n > 1) {
+            sum += spf[n];
+            n /= spf[n];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        sieve(); 
+
+        int N = scanner.nextInt();
+        long sum = 0;
+        while (N-- > 0) {
+            int n = scanner.nextInt();
+            sum += checknum(n);
+        }
+        System.out.println(sum);
+    }
+}
