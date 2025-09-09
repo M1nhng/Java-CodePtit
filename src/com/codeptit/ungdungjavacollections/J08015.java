93a1e017941f7da02b74623dc954764b4a191d63
M1nhng <nguyenchuminh223@gmail.com>
2025-09-09 14:26:37 +0700

    new

--- src/com/codeptit/ungdungjavacollections/J08015.java
+++ src/com/codeptit/ungdungjavacollections/J08015.java
@@ -1,5 +1,7 @@
 package com.codeptit.ungdungjavacollections;
 import java.util.*;
+
+
 public class J08015 {
 	public static void main(String[]args) {
 		Scanner scanner = new Scanner(System.in);
@@ -13,8 +15,9 @@ public class J08015 {
 			int cnt = 0;
 			
 			for(int i = 0; i < n; i++) {
-				a[i] = scanner.nextInt();
+				a[i] = scanner.nextInt();					
 			}
+			
 			for(int i = 0; i < n; i++) {
 				for(int j = i + 1; j < n; j++) {
 					if(Math.abs(a[i]) + Math.abs(a[j]) == k) {
